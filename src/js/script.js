14373e34e695fc3e57482df2401cbf14d9ca8821
Lukhe <akmalthemalong.com>
2024-02-07 21:34:09 +0800

    audo play audio

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const playAudio = document.getElementById('play-audio')
 const audio = document.getElementById('backsound')
 
+audio.play()
 playAudio.addEventListener('click', () => {
   if (audio.paused) {
     audio.play()
